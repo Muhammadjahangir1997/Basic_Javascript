0f691cd0d265ffedf9a691836e19b07b58527430
Muhammad Jahangir <muhammadjahangir1997oo@gmail.com>
2024-06-27 08:54:25 -0700

    add filse

---
+++ 12-If_else.js
@@ -0,0 +1,28 @@
+
+
+let marks = 69;
+
+if (marks >= 70) {
+    console.log(" A Grade");
+    
+}
+else if (marks >= 60) {
+    console.log(" B Grade");
+    
+}
+ else if (marks >= 50) {
+    console.log(" C Grade");
+    
+}
+else if (marks >= 45) {
+    console.log(" D Grade");
+    
+}
+else if (marks >= 40) {
+    console.log(" E Grade");
+    
+}
+else{
+    console.log("Sorry your Fail");
+}
+
