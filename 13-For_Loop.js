085350b40b70d4f9fa39ad5c0ff12c4a5283255d
Muhammad Jahangir <muhammadjahangir1997oo@gmail.com>
2024-06-28 05:46:12 -0700

    add files

---
+++ 13-For_Loop.js
@@ -0,0 +1,16 @@
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+    
+}
+
+
+
+for (let i= 0; i <= 10; i++) {
+    const element = i;
+    if (element === 7) {
+        console.log("7 is a best number");
+    }
+    console.log(element);
+}
+
