ced57e20be811d4d744d55486dd898efdc7fa359
Muhammad Jahangir <muhammadjahangir1997oo@gmail.com>
2024-06-30 06:54:51 -0700

    add files

---
+++ 16-While_Loop.js
@@ -0,0 +1,21 @@
+let i = 0
+while (i <= 10) {
+    console.log(`value of i is ${i}`);
+    i = i + 2
+}
+
+
+
+let MyArry = ["ironman", "badman", "superman"]
+let arr = 0
+while (arr < MyArry.length) {
+   console.log(`value is ${MyArry[arr]}`);
+   arr = arr + 1
+}
+
+
+let score = 1 
+do {
+   console.log(`score is ${score} `);
+   score++
+} while (score <= 10);
