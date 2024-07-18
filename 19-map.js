24e1d58c7f4003a9962ba77f44f7194d24b6746c
Muhammad Jahangir <muhammadjahangir1997oo@gmail.com>
2024-07-18 08:27:45 -0700

    add files

---
+++ 19-map.js
@@ -0,0 +1,11 @@
+const map = new Map ()             // for in loop is not exeptable is code
+map.set('pk',"pakistan")
+map.set('ch',"china")
+map.set('pk',"pakistan")
+map.set('IN',"India")
+
+console.log(map);
+// for (const key in map) {
+//    console.log(key[map],map);
+   
+//  }
---
+++ 20-for_in_of.js
@@ -0,0 +1,17 @@
+const coding =["javascript", "phython", "c++","java"]          //my practice
+
+for (const object of coding) {
+   console.log(object,);
+   const myarr=["js", "phy", "c++","java"]
+for (const value of myarr) {
+  console.log(value , coding);
+   }
+    
+}
+
+
+
+
+
+
+
